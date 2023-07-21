63651d9977f28aaea85b3bfc088738c6fa36e5ee
sreenathprem <sreenath.premnadh@hashicorp.com>
2023-07-21 15:17:27 +0200

    Reverting to Vault TF provider

--- main.tf
+++ main.tf
@@ -13,6 +13,16 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
+  client_id     = data.vault_azure_access_credentials.creds.client_id
+  client_secret = data.vault_azure_access_credentials.creds.client_secret
+}
+
+data "vault_azure_access_credentials" "creds" {
+  role                        = "edu-app"
+  validate_creds              = true
+  num_sequential_successes    = 8
+  num_seconds_between_tests   = 1
+  max_cred_validation_seconds = 300 
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
