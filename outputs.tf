44b75be232f634dde9515436ba1785772c7728b0
sreenathprem <sreenath.premnadh@hashicorp.com>
2023-07-28 13:52:03 +0200

    Commenting out outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,7 @@
-# Outputs file
-output "catapp_url" {
-  value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
-}
+# output "catapp_url" {
+#   value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
+# }
 
-output "catapp_ip" {
-  value = "http://${azurerm_public_ip.catapp-pip.ip_address}"
-}
+# output "catapp_ip" {
+#   value = "http://${azurerm_public_ip.catapp-pip.ip_address}"
+# }
